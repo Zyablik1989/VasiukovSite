958a2cd13430a768be301e4f0d87a4d9cef5314b
1 <1@1.com>
2025-04-22 12:16:34 +0100

    added component that should be used

---
+++ VasiukovSite.FrontEndAngular/src/app/cheatsheet/cheatsheet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cheatsheet',
+  standalone: true,
+  imports: [],
+  templateUrl: './cheatsheet.component.html',
+  styleUrl: './cheatsheet.component.css'
+})
+export class CheatsheetComponent {
+
+}
