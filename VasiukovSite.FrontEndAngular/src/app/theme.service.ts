5b87188a8f70fe803ff8797a368da5ab7b0bc5c2
1 <1@1.com>
2025-04-28 00:36:03 +0100

    dark theme is saved in localStorage

--- VasiukovSite.FrontEndAngular/src/app/theme.service.ts
+++ VasiukovSite.FrontEndAngular/src/app/theme.service.ts
@@ -5,20 +5,41 @@ import {Injectable, Renderer2, RendererFactory2 } from '@angular/core';
 })
 export class ThemeService {
   private renderer: Renderer2;
-  private currentTheme = 'dark-theme';
+  private currentTheme: 'light-theme' | 'dark-theme' = 'dark-theme';
+
   constructor(rendererFactory: RendererFactory2) {
     this.renderer = rendererFactory.createRenderer(null, null);
+
+
+  const savedTheme = localStorage.getItem('app-theme') as 'light-theme' | 'dark-theme' | null;
+
+  if (savedTheme){
+    this.currentTheme = savedTheme;
+
   }
+  this.applyTheme(this.currentTheme);
+}
 
 
 
   setTheme(theme: 'light-theme' | 'dark-theme'){
-    const body = document.body;
-    // const a = document.
-    this.renderer.removeClass(body, this.currentTheme);
-    this.renderer.addClass(body,theme);
+    if (this.currentTheme === theme) return;
+
+    this.removeTheme(this.currentTheme);
+    this.applyTheme(theme);
     this.currentTheme = theme;
+    localStorage.setItem('app-theme', theme);
   }
+
+private applyTheme(theme: 'light-theme' | 'dark-theme'){
+  const body = document.body;
+  this.renderer.addClass(body, theme);
+}
+
+private removeTheme(theme: 'light-theme' | 'dark-theme'){
+  const body = document.body;
+  this.renderer.removeClass(body,theme);
+}
 
   getCurrentTheme(): string {
     return this.currentTheme;
