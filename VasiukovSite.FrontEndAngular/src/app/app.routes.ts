24c007fb2868ec216a31ed897df2a8a824840769
alex <AlekseiVasiukovWork@gmail.com>
2025-06-28 15:08:22 +0100

    404 page component

--- VasiukovSite.FrontEndAngular/src/app/app.routes.ts
+++ VasiukovSite.FrontEndAngular/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { ProfileComponent } from './profile/profile.component';
 import { CheatsheetComponent } from './cheatsheet/cheatsheet.component';
 import { BlogComponent } from './blog/blog.component';
 import { ApplicationConfig } from '@angular/core';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-  // {path: '', component: ProfileComponent},
+  {path: '', component: ProfileComponent},
   {path: 'profile', component: ProfileComponent},
   {path: 'cheatsheet', component: CheatsheetComponent},
-  {path: 'blog', component: BlogComponent}
+  {path: 'blog', component: BlogComponent},
+  { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404
 
 ];
 
